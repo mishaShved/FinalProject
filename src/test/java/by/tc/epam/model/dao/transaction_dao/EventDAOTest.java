393665986e34387722adf71fce153d0be5e83519
misha <mischa.schwed@gmail.com>
2018-05-08 17:48:33 +0300

    +log

--- src/test/java/by/tc/epam/model/dao/transaction_dao/EventDAOTest.java
+++ src/test/java/by/tc/epam/model/dao/transaction_dao/EventDAOTest.java
@@ -1,25 +1,16 @@
 package by.tc.epam.model.dao.transaction_dao;
 
-import by.tc.epam.model.dao.connection_pool.ConnectionPool;
-import by.tc.epam.model.dao.exception.ConnectionPoolException;
-import org.junit.BeforeClass;
+import by.tc.epam.model.dao.exception.DAOSQLException;
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+
 import java.sql.SQLException;
 
 public class EventDAOTest {
 
-    private static Connection getH2Connection() throws SQLException {
-        return DriverManager.getConnection("jdbc:h2:mem:testDB");
-    }
 
     @Test
-    public void createEventTest() throws SQLException {
-
-        Connection conn = getH2Connection();
-
+    public void createEventTest() throws SQLException, DAOSQLException {
 
 
     }
